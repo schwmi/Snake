fcbfffe8d7807071e9c156ddec826e08e02f2a7d
Michael Schwarz <m.schwarz@pocketscience.at>
2013-04-19 09:48:39 +0200

    Initial Commit

---
+++ Snake/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  Snake
+//
+//  Created by Michael Schwarz on 19.04.13.
+//  Copyright (c) 2013 Michael Schwarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Snake/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Snake
+//
+//  Created by Michael Schwarz on 19.04.13.
+//  Copyright (c) 2013 Michael Schwarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
